436822b1af79c0298d50cf488b5a0cae07059a2f
ackerman <jaime_jj@hotmail.com>
2020-11-11 19:29:36 +0100

    Test
    Test

---
+++ esp32/TinyZXESPectrumttgovga32/ZX-ESPectrum/dataFlash/gbsna.h
@@ -0,0 +1,56 @@
+#ifndef GB_SNA_H
+ #define GB_SNA_H
+ 
+ #include "sna/snaDiag48K.h"
+ #include "sna/snaFantasy48K.h"
+ #include "sna/snaSPpong48K.h"
+ //#include "sna/snaManic48K.h"
+ #include "sna/snaFire48K.h"
+ //#include "sna/snaSilkwormKM48K.h"
+ #include "sna/sna3Dcoolkm48K.h"
+ #include "sna/snaAT4OPENKM48K.h"
+ //#include "sna/snaBREDITORKM48K.h"
+ #include "sna/snaSIPKA48K.h"
+ #include "sna/snaMouseTestZxds48K.h"
+ //#include "sna/snaArkanoid48K.h" 
+
+ #define max_list_sna_48 8
+// #define max_list_sna_48 1 
+ 
+ //sna 48K 
+ //Titulos
+ static const char * gb_list_sna_48k_title[max_list_sna_48]={
+  "diag",
+  "fantasy",
+  "sppong",
+  //"manic",
+  "fire",
+  //"Arkanoid",
+  //"SilkWormKM",
+  "3DcoolKm",
+  "AT4openKM",
+  //"BrEditorKM",
+  "Sipka",
+  "MouseTestZxds"
+  //"EightOfNoise",
+  //"WWTF"
+ };
+ 
+ //Datos 48K sna
+ static const unsigned char * gb_list_sna_48k_data[max_list_sna_48]={
+  gb_sna_diag_48k,
+  gb_sna_fantasy_48k,
+  gb_sna_sppong_48k,
+  //gb_sna_manic_48k,
+  gb_sna_fire_48k,
+  //gb_sna_arkanoid_48k,
+  //gb_sna_SILKWORMKM_48k,
+  gb_3dcoolkm_48k,
+  gb_sna_AT4OPENKM_48k,
+  //gb_sna_BREDITORKM_48k,
+  gb_sna_SIPKA_48K_H_48k,
+  gb_sna_mouseTestZxds_48k
+//  gb_sna_eightOfNoise_48k,
+//  gb_sna_wwtf_48k
+ };
+#endif 
