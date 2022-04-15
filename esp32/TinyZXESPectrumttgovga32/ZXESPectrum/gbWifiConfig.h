0af6c64c04f99543e1fd0b693d7de80068f82b21
ackerman <jaime_jj@hotmail.com>
2022-04-15 18:14:18 +0200

    Fix Load WIFI

--- esp32/TinyZXESPectrumttgovga32/ZXESPectrum/gbWifiConfig.h
+++ esp32/TinyZXESPectrumttgovga32/ZXESPectrum/gbWifiConfig.h
@@ -7,9 +7,20 @@
  //#define gb_wifi_url_base_path "http://192.168.0.36/zxspectrum/output"
  #define gb_wifi_url_base_path "https://rpsubc8.github.io/ESP32TinyZXSpectrum/www/zxspectrum/output"
 
+
+ //Delay check wifi connect
+ #define gb_wifi_delay_check 500
+
+ //Delay millis asign http and read data
+ #define gb_wifi_delay_asign_read 500
+
+ //Delay millis in check available buffer
+ #define gb_wifi_delay_check_available 100
+
  //millisecons delay stream read
  #define gb_wifi_delay_available 0
 
+ 
  //#define use_lib_wifi_debug
 
 #endif
