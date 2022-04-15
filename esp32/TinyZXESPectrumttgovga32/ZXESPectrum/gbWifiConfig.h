12a24469fae225b8ecacbdda131f6d5543db0884
ackerman <jaime_jj@hotmail.com>
2022-04-15 11:39:18 +0200

    WIFI Support 48k

---
+++ esp32/TinyZXESPectrumttgovga32/ZXESPectrum/gbWifiConfig.h
@@ -0,0 +1,16 @@
+#ifndef _GB_WIFI_CONFIG_H
+ #define _GB_WIFI_CONFIG_H
+
+ #define gb_wifi_ssd "nombreDeNuestraRedWIFIdelRooter"
+ #define gb_wifi_pass "passwordDeNuestraRedWIFIdelRooter"
+
+ //#define gb_wifi_url_base_path "http://192.168.0.36/zxspectrum/output"
+ #define gb_wifi_url_base_path "https://rpsubc8.github.io/ESP32TinyZXSpectrum/www/zxspectrum/output"
+
+ //millisecons delay stream read
+ #define gb_wifi_delay_available 0
+
+ //#define use_lib_wifi_debug
+
+#endif
+
