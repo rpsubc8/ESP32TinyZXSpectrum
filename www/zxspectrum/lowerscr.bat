3874901a4e7118c99e76ebb0a972dda894070d63
ackerman <jaime_jj@hotmail.com>
2022-04-15 10:17:19 +0200

    Tool support WIFI

---
+++ www/zxspectrum/lowerscr.bat
@@ -0,0 +1,23 @@
+@echo off
+ren output\outdat\scr\action\*.SCR *.scr
+ren output\outdat\scr\arcade\*.SCR *.scr
+ren output\outdat\scr\board\*.SCR *.scr
+ren output\outdat\scr\card\*.SCR *.scr
+ren output\outdat\scr\chess\*.SCR *.scr
+ren output\outdat\scr\combat\*.SCR *.scr
+ren output\outdat\scr\dungeon\*.SCR *.scr
+ren output\outdat\scr\fight\*.SCR *.scr
+ren output\outdat\scr\maze\*.SCR *.scr
+ren output\outdat\scr\pinball\*.SCR *.scr
+ren output\outdat\scr\platform\*.SCR *.scr
+ren output\outdat\scr\pong\*.SCR *.scr
+ren output\outdat\scr\puzzle\*.SCR *.scr
+ren output\outdat\scr\race\*.SCR *.scr
+ren output\outdat\scr\rpg\*.SCR *.scr
+ren output\outdat\scr\scene\*.SCR *.scr
+ren output\outdat\scr\shoot\*.SCR *.scr
+ren output\outdat\scr\simul\*.SCR *.scr
+ren output\outdat\scr\sport\*.SCR *.scr
+ren output\outdat\scr\strategy\*.SCR *.scr
+ren output\outdat\scr\venture\*.SCR *.scr
+ren output\outdat\scr\war\*.SCR *.scr
